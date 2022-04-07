4a953891ccd08bad36b3fcc51e3029d8fc1ede9f
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-08 01:03:26 +0430

    listen for client

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,7 +1,38 @@
 package client
 
-import "github.com/streadway/amqp"
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
 
 type Client struct {
 	Connection *amqp.Connection
+}
+
+func (c *Client) Listen() error {
+	ch, err := c.Connection.Channel()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		err := ch.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	msgs, err := ch.Consume("Queue", "", true, false, false, false, nil)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			fmt.Printf("%v \n", d)
+		}
+	}()
+
+	<-forever
+
+	return nil
 }
