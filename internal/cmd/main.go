5c01ecc6dacb84b1319555617ea5b7a85bc17199
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-08 01:56:00 +0430

    main refactor

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -1,5 +1,25 @@
 package cmd
 
-func Execute() {
+import (
+	"github.com/amirhnajafiz/playful-rabbit/internal/client"
+	"github.com/amirhnajafiz/playful-rabbit/internal/rabbitMQT"
+)
 
+func Execute() {
+	{
+		r, _ := rabbitMQT.Init(rabbitMQT.Config{})
+		cli := client.Client{
+			Connection: r,
+		}
+
+		_ = cli.Listen()
+	}
+	{
+		r, _ := rabbitMQT.Init(rabbitMQT.Config{})
+		cli := client.Client{
+			Connection: r,
+		}
+
+		_ = cli.Push()
+	}
 }
