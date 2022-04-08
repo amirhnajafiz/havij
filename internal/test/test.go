c15bd783d121649a41d97c28e67bcaec96c500bf
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-08 13:23:04 +0430

    testing methods init

--- internal/test/test.go
+++ internal/test/test.go
@@ -2,8 +2,24 @@ package test
 
 import "time"
 
+var tests []*Test
+
 type Test struct {
 	Id      string
 	Content string
 	Start   time.Time
+}
+
+func New(test *Test) {
+	tests = append(tests, test)
+}
+
+func Get(Id string) *Test {
+	for _, test := range tests {
+		if test.Id == Id {
+			return test
+		}
+	}
+
+	return nil
 }
