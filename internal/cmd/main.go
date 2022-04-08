bd1f030e956dff13884d3d51b5def0583915a4b7
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-08 13:13:26 +0430

    config added in main

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -2,21 +2,26 @@ package cmd
 
 import (
 	"github.com/amirhnajafiz/playful-rabbit/internal/client"
+	"github.com/amirhnajafiz/playful-rabbit/internal/config"
 	"github.com/amirhnajafiz/playful-rabbit/internal/rabbitMQT"
 )
 
 func Execute() {
+	c := config.Load()
+
 	{
-		r, _ := rabbitMQT.Init(rabbitMQT.Config{})
+		r, _ := rabbitMQT.Init(c.Rabbit)
 		cli := client.Client{
+			Cfg:        c.Client,
 			Connection: r,
 		}
 
 		_ = cli.Listen()
 	}
 	{
-		r, _ := rabbitMQT.Init(rabbitMQT.Config{})
+		r, _ := rabbitMQT.Init(c.Rabbit)
 		cli := client.Client{
+			Cfg:        c.Client,
 			Connection: r,
 		}
 
