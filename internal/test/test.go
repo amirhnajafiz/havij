57aec8c5ce20e591b56780feb46e949ab52ffa6f
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-10 03:25:42 +0430

    tests methods refactor

--- internal/test/test.go
+++ internal/test/test.go
@@ -19,11 +19,11 @@ type Test struct {
 	Duration time.Duration
 }
 
-func New(test *Test) {
+func create(test *Test) {
 	tests = append(tests, test)
 }
 
-func Find(Id string) bool {
+func find(Id string) bool {
 	for _, test := range tests {
 		if test.Id == Id {
 			return true
@@ -34,6 +34,10 @@ func Find(Id string) bool {
 }
 
 func Done(Id string) bool {
+	if !find(Id) {
+		return false
+	}
+
 	for _, test := range tests {
 		if test.Id == Id {
 			test.Duration = test.Start.Sub(time.Now())
@@ -51,14 +55,12 @@ func Done(Id string) bool {
 
 func Generate(number int) []*Test {
 	for i := 0; i < number; i++ {
-		temp := Test{
+		create(&Test{
 			Id:      time.UnixMilli(int64(i)).String(),
 			Content: faker.Sentence(), // random text
 			Start:   time.Now(),
 			Missed:  false,
-		}
-
-		New(&temp)
+		})
 	}
 
 	return tests
