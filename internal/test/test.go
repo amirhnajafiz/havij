e17407e1fb7e30ea2748d7bb9badda8663a65c89
amirhnajafiz <najafizadeh21@gmail.com>
2022-04-09 12:40:11 +0430

    test done method implemented

--- internal/test/test.go
+++ internal/test/test.go
@@ -5,16 +5,17 @@ import "time"
 var tests []*Test
 
 type Test struct {
-	Id      string
-	Content string
-	Start   time.Time
+	Id       string
+	Content  string
+	Start    time.Time
+	Duration time.Duration
 }
 
 func New(test *Test) {
 	tests = append(tests, test)
 }
 
-func Get(Id string) *Test {
+func Find(Id string) *Test {
 	for _, test := range tests {
 		if test.Id == Id {
 			return test
@@ -22,4 +23,16 @@ func Get(Id string) *Test {
 	}
 
 	return nil
+}
+
+func Done(Id string) bool {
+	for _, test := range tests {
+		if test.Id == Id {
+			test.Duration = test.Start.Sub(time.Now())
+
+			return true
+		}
+	}
+
+	return false
 }
